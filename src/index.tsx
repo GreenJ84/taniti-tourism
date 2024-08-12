e9bc652cd4ef4a4b421eff86b3d3aad8de251a89
Jesse Greenough <jesse.greenough84@gmail.com>
2024-08-12 08:53:54 -0700

    Create each page component template and integrate in application routing system

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+
+import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Activities from './pages/Activities';
+import Cuisine from './pages/Cuisine';
+import FAQ from './pages/FAQ';
+import Lodging from './pages/Lodging';
+import Transportation from './pages/Transportation';
+import NotFound from './pages/NotFound';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="activities" element={<Activities />} />
+          <Route path="cuisine" element={<Cuisine />} />
+          <Route path="Lodging" element={<FAQ />} />
+          <Route path="Transportation" element={<Lodging />} />
+          <Route path="FAQ" element={<Transportation />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
