7b71c44556a026182560631f6ab08418bd545051
Jesse Greenough <jesse.greenough84@gmail.com>
2024-08-14 05:10:05 -0700

    Create utility Navigational link list for components

---
+++ src/utils/NavLinks.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+interface INavLink {
+  name: string;
+  link: string;
+}
+
+const navLinkData: INavLink[] = [
+  { name: "Home", link: "/"},
+  { name: "About", link: "/About"},
+  { name: "Activities", link: "/Activities"},
+  { name: "Cuisine", link: "/Cuisine"},
+  { name: "Lodging", link: "/Lodging"},
+  { name: "Transportation", link: "/Transportation"},
+  { name: "FAQ", link: "/FAQ"},
+]
+
+const navLinks = () => {
+  return (
+    <>
+    {navLinkData.map((link, index) => (
+      <li key={index}>
+        <a href={link.link}>
+          {link.name}
+        </a>
+      </li>
+    ))}
+    </>
+  )
+}
+
+export default navLinks;
