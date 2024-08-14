7f8540fe025ae85f8b0ffa0641843bd8b7cce38e
Jesse Greenough <jesse.greenough84@gmail.com>
2024-08-14 05:10:22 -0700

    Develop and style simple Header and Footer sections

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
+import NavLinks from '../utils/NavLinks'
+import styled from 'styled-components';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Foot>
+      <nav>
+        <ul>
+          <NavLinks />
+        </ul>
+      </nav>
+    </Foot>
   )
 }
 
-export default Footer
+export default Footer;
+
+const Foot = styled.footer`
+
+  width: 100vw;
+  height: 6dvh;
+  background-color: rgb(var(--secondary-bg));
+  > nav > ul {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
--- src/shared/Navigation.tsx
+++ src/shared/Navigation.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import NavLinks from '../utils/NavLinks'
+import { Link, useLocation } from 'react-router-dom'
+import styled from 'styled-components'
+import { AppContext } from '../utils/AppContext'
 
 const Navigation = () => {
+  const location = useLocation();
+
+  const isHomePage = location.pathname === '/';
   return (
-    <div>Navigation</div>
+    <Header>
+      <nav>
+        <Link to="/" id="brand">
+          <img src="taniti-logo.avif" alt="Taniti Brand" />
+        </Link>
+        {isHomePage ?
+          <></> :
+          <ul>
+            <NavLinks />
+          </ul>
+        }
+      </nav>
+    </Header>
   )
 }
 
-export default Navigation
+export default Navigation;
+
+const Header = styled.header`
+  width: 100vw;
+  height: 12dvh;
+  background-color: rgb(var(--secondary-bg));
+  > #brand, #brand > img {
+    max-width: 20vw;
+  }
+  > nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 10px;
+  }
+`;
